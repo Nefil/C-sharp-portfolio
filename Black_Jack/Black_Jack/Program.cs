8fd28dd1ef9e964405ad2468a48ff3dd7c52881f
Nefil <daniel.segeth@gmail.com>
2025-07-15 17:58:42 +0200

    Update Program.cs

--- Black_Jack/Black_Jack/Program.cs
+++ Black_Jack/Black_Jack/Program.cs
@@ -1,138 +1,225 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-int wallet = 400; 
-bool wantsToPlay = true; 
-
-// Dictionary to hold the card values
-Dictionary<string, int> cardValues = new Dictionary<string, int>()
+class Program
 {
-    { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 },
-    { "6", 6 }, { "7", 7 }, { "8", 8 }, { "9", 9 },
-    { "10", 10 }, { "J", 10 }, { "Q", 10 }, { "K", 10 },
-    { "A", 11 }
-};
-
-Random random = new Random();
-
-Console.WriteLine("Welcome to Blackjack!");
-Console.WriteLine($"Your starting wallet amount is: {wallet}$");
-Console.WriteLine("Good luck and enjoy your game. You need 200$ to start.");
-Console.WriteLine("Type 'start' and press Enter to begin.");
-
-string startGame = Console.ReadLine();
-
-if (startGame.ToLower() == "start")
-{
-    while (wallet >= 200 && wantsToPlay)
+    static void Main()
     {
-        wallet -= 200;
+        int wallet = 400;
+        bool wantsToPlay = true;
 
-        List<KeyValuePair<string, int>> playerHand = new List<KeyValuePair<string, int>>();
-        List<KeyValuePair<string, int>> dealerHand = new List<KeyValuePair<string, int>>();
-
-        for (int i = 0; i < 2; i++)
+        Dictionary<string, int> cardValues = new Dictionary<string, int>()
         {
-            var playerCard = cardValues.ElementAt(random.Next(cardValues.Count));
-            playerHand.Add(playerCard);
+            { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 },
+            { "6", 6 }, { "7", 7 }, { "8", 8 }, { "9", 9 },
+            { "10", 10 }, { "J", 10 }, { "Q", 10 }, { "K", 10 },
+            { "A", 11 }
+        };
 
-            var dealerCard = cardValues.ElementAt(random.Next(cardValues.Count));
-            dealerHand.Add(dealerCard);
-        }
+        Random random = new Random();
 
-        // Dealer randomly draws a third card or not
-        int dealer_choice = random.Next(1, 3);
-        if (dealer_choice == 1)
+        Console.WriteLine("Welcome to Blackjack!");
+        Console.WriteLine($"Your starting wallet amount is: {wallet}$");
+        Console.WriteLine("Good luck and enjoy your game. You need 200$ to start.");
+        Console.WriteLine("Type 'start' and press Enter to begin.");
+
+        string startGame = Console.ReadLine()?.Trim();
+
+        if (startGame?.ToLower() == "start")
         {
-            var newCardDealer = cardValues.ElementAt(random.Next(cardValues.Count));
-            dealerHand.Add(newCardDealer);
-        }
-
-        Console.WriteLine("Your hand:");
-        foreach (var card in playerHand)
-        {
-            Console.WriteLine($"- {card.Key}");
-        }
-
-        // Pokazujemy jedną kartę dealera
-        Console.WriteLine($"Dealer shows: {dealerHand[0].Key}");
-
-        Console.WriteLine("Do you want to choose another card? yes/no");
-        string choice = Console.ReadLine();
-
-        if (choice.ToLower() == "yes")
-        {
-            var newCard = cardValues.ElementAt(random.Next(cardValues.Count));
-            playerHand.Add(newCard);
-
-            Console.WriteLine("Your hand:");
-            foreach (var card in playerHand)
+            while (wallet >= 200 && wantsToPlay)
             {
-                Console.WriteLine($"- {card.Key}");
+                wallet -= 200;
+                List<KeyValuePair<string, int>> playerHand = new List<KeyValuePair<string, int>>();
+                List<KeyValuePair<string, int>> dealerHand = new List<KeyValuePair<string, int>>();
+
+                // Rozdanie początkowe
+                for (int i = 0; i < 2; i++)
+                {
+                    var playerCard = cardValues.ElementAt(random.Next(cardValues.Count));
+                    playerHand.Add(playerCard);
+
+                    var dealerCard = cardValues.ElementAt(random.Next(cardValues.Count));
+                    dealerHand.Add(dealerCard);
+                }
+
+                // Sprawdzenie blackjacka
+                int playerSum = SumHand(playerHand);
+                int dealerSum = SumHand(dealerHand);
+
+                bool playerBlackjack = (playerHand.Count == 2 && playerSum == 21);
+                bool dealerBlackjack = (dealerHand.Count == 2 && dealerSum == 21);
+
+                Console.WriteLine("\nYour hand:");
+                foreach (var card in playerHand)
+                    Console.WriteLine($"- {card.Key}");
+
+                Console.WriteLine($"Dealer shows: {dealerHand[0].Key}");
+
+                if (playerBlackjack || dealerBlackjack)
+                {
+                    if (playerBlackjack && dealerBlackjack)
+                    {
+                        Console.WriteLine("Both have blackjack! It's a tie! You get your money back.");
+                        wallet += 200;
+                    }
+                    else if (playerBlackjack)
+                    {
+                        Console.WriteLine("Blackjack! You win and gain 300$.");
+                        wallet += 500; // 200$ zwrot stawki + 300$ wygrana
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dealer has blackjack! You lose 200$.");
+                    }
+                    ShowWallet(wallet);
+                    if (!PlayAgainPrompt(ref wantsToPlay)) break;
+                    continue;
+                }
+
+                // Dobieranie przez gracza
+                bool playerTurn = true;
+                while (playerTurn)
+                {
+                    Console.WriteLine("Do you want to choose another card? yes/no");
+                    string choice = Console.ReadLine()?.Trim().ToLower();
+                    if (choice == "yes")
+                    {
+                        var newCard = cardValues.ElementAt(random.Next(cardValues.Count));
+                        playerHand.Add(newCard);
+                        Console.WriteLine("Your hand:");
+                        foreach (var card in playerHand)
+                            Console.WriteLine($"- {card.Key}");
+
+                        FixAces(playerHand);
+                        playerSum = SumHand(playerHand);
+
+                        if (playerSum > 21)
+                        {
+                            Console.WriteLine("You busted!");
+                            playerTurn = false;
+                        }
+                    }
+                    else if (choice == "no")
+                    {
+                        playerTurn = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please type 'yes' or 'no'.");
+                    }
+                }
+
+                FixAces(playerHand);
+                playerSum = SumHand(playerHand);
+
+                // Dobieranie przez dealera (do minimum 17)
+                FixAces(dealerHand);
+                dealerSum = SumHand(dealerHand);
+                while (dealerSum < 17)
+                {
+                    var newCardDealer = cardValues.ElementAt(random.Next(cardValues.Count));
+                    dealerHand.Add(newCardDealer);
+                    FixAces(dealerHand);
+                    dealerSum = SumHand(dealerHand);
+                }
+
+                // Pokazanie rąk
+                Console.WriteLine($"\nYour total: {playerSum}");
+                Console.WriteLine("Dealer's hand:");
+                foreach (var card in dealerHand)
+                    Console.WriteLine($"- {card.Key}");
+                Console.WriteLine($"Dealer's total: {dealerSum}");
+
+                // Wynik gry
+                if (playerSum > 21)
+                {
+                    Console.WriteLine("You busted! You lose 200$.");
+                }
+                else if (dealerSum > 21)
+                {
+                    Console.WriteLine("Dealer busted! You win 200$.");
+                    wallet += 400;
+                }
+                else if (playerSum == dealerSum)
+                {
+                    Console.WriteLine("It's a tie! You get your money back.");
+                    wallet += 200;
+                }
+                else if (playerSum > dealerSum)
+                {
+                    Console.WriteLine("You win! You gain 200$.");
+                    wallet += 400;
+                }
+                else
+                {
+                    Console.WriteLine("Dealer wins! You lose 200$.");
+                }
+
+                ShowWallet(wallet);
+
+                if (wallet < 200)
+                {
+                    Console.WriteLine("You don't have enough money to play again. The game ends.");
+                    break;
+                }
+
+                if (!PlayAgainPrompt(ref wantsToPlay))
+                {
+                    Console.WriteLine($"Thank you for playing! Your final wallet amount is: {wallet}$");
+                    break;
+                }
             }
-        }
-
-        // Zmiana wartości Asa u gracza, jeśli potrzebne
-        if (playerHand.Sum(card => card.Value) > 21)
-        {
-            var aceIndex = playerHand.FindIndex(card => card.Key == "A" && card.Value == 11);
-            if (aceIndex != -1)
-            {
-                playerHand[aceIndex] = new KeyValuePair<string, int>("A", 1);
-            }
-        }
-
-        // Zmiana wartości Asa u dealera, jeśli potrzebne
-        if (dealerHand.Sum(card => card.Value) > 21)
-        {
-            var aceIndex = dealerHand.FindIndex(card => card.Key == "A" && card.Value == 11);
-            if (aceIndex != -1)
-            {
-                dealerHand[aceIndex] = new KeyValuePair<string, int>("A", 1);
-            }
-        }
-
-        int yourcardsum = playerHand.Sum(card => card.Value);
-        int dealercardsum = dealerHand.Sum(card => card.Value);
-
-        Console.WriteLine($"Your total: {yourcardsum}");
-        Console.WriteLine("Dealer's hand:");
-        foreach (var card in dealerHand)
-        {
-            Console.WriteLine($"- {card.Key}");
-        }
-        Console.WriteLine($"Dealer's total: {dealercardsum}");
-
-        if (yourcardsum == dealercardsum)
-        {
-            Console.WriteLine("It's a tie! You get your money back.");
-            wallet += 200;
-        }
-        else if (yourcardsum > 21)
-        {
-            Console.WriteLine("You busted! You lose 200$.");
-        }
-        else if (dealercardsum > 21 || yourcardsum > dealercardsum)
-        {
-            Console.WriteLine("You win! You gain 200$.");
-            wallet += 400;
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(intercept: true);
         }
         else
         {
-            Console.WriteLine("Dealer wins! You lose 200$.");
-        }
-
-        Console.WriteLine($"Your current wallet amount is: {wallet}$");
-        Console.WriteLine("Do you want to play again? yes/no");
-        string ending = Console.ReadLine();
-
-        if (ending.ToLower() != "yes")
-        {
-            wantsToPlay = false;
-            Console.WriteLine($"Thank you for playing! Your final wallet amount is: {wallet}$");
+            Console.WriteLine("Game did not start. Please type 'start' next time.");
         }
     }
 
-    Console.ReadKey(intercept: true);
+    static int SumHand(List<KeyValuePair<string, int>> hand)
+    {
+        return hand.Sum(card => card.Value);
+    }
+
+    static void FixAces(List<KeyValuePair<string, int>> hand)
+    {
+        while (SumHand(hand) > 21 && hand.Any(card => card.Key == "A" && card.Value == 11))
+        {
+            int aceIndex = hand.FindIndex(card => card.Key == "A" && card.Value == 11);
+            if (aceIndex != -1)
+                hand[aceIndex] = new KeyValuePair<string, int>("A", 1);
+        }
+    }
+
+    static void ShowWallet(int wallet)
+    {
+        Console.WriteLine($"\nYour current wallet amount is: {wallet}$");
+    }
+
+    static bool PlayAgainPrompt(ref bool wantsToPlay)
+    {
+        while (true)
+        {
+            Console.WriteLine("Do you want to play again? yes/no");
+            string ending = Console.ReadLine()?.Trim().ToLower();
+            if (ending == "yes")
+            {
+                wantsToPlay = true;
+                return true;
+            }
+            else if (ending == "no")
+            {
+                wantsToPlay = false;
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Please type 'yes' or 'no'.");
+            }
+        }
+    }
 }
