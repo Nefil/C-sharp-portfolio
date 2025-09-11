f945595e229fec05aacaddb6e692372727b5b9c8
Daniel Segeth <daniel.segeth@gmail.com>
2025-09-11 12:24:16 +0200

    Dodaj pliki projektów.

---
+++ MiniShop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ PatternLibraryDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PatternLibraryDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
