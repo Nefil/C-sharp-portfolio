7d8e96f589f304bd7677a8baa7d49704821d64cf
Daniel Segeth <daniel.segeth@gmail.com>
2025-09-11 20:28:08 +0200

    creating a deck of cards and a card class, creating methods for shuffling and laying 3 cards on the table

---
+++ PokerCardGame/Deck.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerCardGame
+{
+    internal class Deck
+    {
+        public Random random = new Random();
+        private List<Card> cards;
+        private List<Card> Table;
+
+        public Deck()
+        {
+            cards = new List<Card>();
+            Table = new List<Card>(); 
+            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+
+            foreach (string suit in suits)
+            {
+                for (int rank = 2; rank <= 14; rank++)
+                {
+                    cards.Add(new Card(rank, suit));
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            for (int i = cards.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(0, i + 1);
+
+                Card temp = cards[i];
+                cards[i] = cards[j];
+                cards[j] = temp;
+
+            }
+
+        }
+
+        public void DealToTable()
+        {      
+            for (int i = 0; i < 3; i++)
+            {   
+                Table.Add(cards[0]);
+                cards.RemoveAt(0);
+            }
+
+            Console.WriteLine("Cards on deck:");
+            foreach (Card card in Table)
+            {
+                Console.WriteLine($"{card.rank} of {card.suit}");
+            }
+        }
+    }
+}
--- PokerCardGame/Program.cs
+++ PokerCardGame/Program.cs
@@ -2,9 +2,13 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            Deck deck = new Deck();
+            deck.Shuffle();
+            deck.DealToTable();
         }
     }
+
 }
